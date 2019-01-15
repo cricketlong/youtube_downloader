f867ae6a9701cad8568d11c49a5c179b18be4dac
Junlin Huang <cricketlong@gmail.com>
2019-01-15 21:19:39 +0000

    fix download error

--- downloader/views.py
+++ downloader/views.py
@@ -11,18 +11,13 @@ def index(request):
 
     if request.method == "POST":
         data["download_url"] = request.POST.get("download_url")
-        data["filename"] = request.POST.get("filename")
         file_path = "/tmp/1.mp3"
         command = '/usr/bin/youtube2mp3 "{0}" "{1}"'.format(data["download_url"], file_path)
         subprocess.call(command, shell=True)
 
         with open(file_path, "w+") as f:
-            response = HttpResponse(f, content_type='application/force-download')
+            response = HttpResponse(f.read(), content_type='application/force-download')
             response['Content-Length'] = len(response.content)
-            custom_filename = "1"
-            if len(data['filename']) > 0:
-                custom_filename = data['filename']
-            print(custom_filename)
-            response['Content-Disposition'] = "attachment; filename=1.mp3".format(custom_filename)
+            response['Content-Disposition'] = "attachment; filename=1.mp3"
 
     return response
