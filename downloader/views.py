b9a47a95de1c39357067753e16b38d27242830ba
Junlin Huang <cricketlong@gmail.com>
2019-01-15 22:19:06 +0000

    fix error...

--- downloader/views.py
+++ downloader/views.py
@@ -3,21 +3,28 @@ from django.http import HttpResponse
 from django.template import loader
 from mimetypes import guess_type
 import subprocess
+import os
 
 def index(request):
     data = {}
-    template = loader.get_template("index.html")
-    response = HttpResponse(template.render(data, request))
+    response = HttpResponse()
 
     if request.method == "POST":
         data["download_url"] = request.POST.get("download_url")
         file_path = "/tmp/1.mp3"
         command = '/usr/bin/youtube2mp3 "{0}" "{1}"'.format(data["download_url"], file_path)
+        print("Command: " + command)
         subprocess.call(command, shell=True)
 
-        with open(file_path, "w+") as f:
-            response = HttpResponse(f.read(), content_type='application/force-download')
-            response['Content-Length'] = len(response.content)
+        with open(file_path, "rb") as f:
+            response = HttpResponse(f.read(), content_type=guess_type(file_path))
+            response['Content-Length'] = os.path.getsize(file_path)
             response['Content-Disposition'] = "attachment; filename=1.mp3"
+            print("File length: " + str(os.path.getsize(file_path)))
+            print(response)
+
+    else:
+        template = loader.get_template("index.html")
+        response = HttpResponse(template.render(data, request))
 
     return response
